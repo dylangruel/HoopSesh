02ac0430deabed31d535dd4462d8191491ab5040
dylangruel <dylangruel@gmail.com>
2023-10-02 12:22:58 -0700

    intro

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+// for running project on Expo Go
+// command prompt -> "cd FirstProject" -> "npm start"
+
+//git add *
+//git commit -m "Changed app file"
+//git push origin master
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>HoopSesh</Text>
+
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +24,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title : {
+    fontSize: 30 , 
+    color: "black" ,
+    fontWeight: "bold" ,
+  },
+  buttonContainer: {
+    marginTop: 20, // Add margin top to move the button down
+    backgroundColor: 'blue',
+    paddingVertical: 15,
+    paddingHorizontal: 50,
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
